1d4dc2e14a18dbc2cdeecde28021d76f9a6a293e
maj-jinnah <maj.jinnah1999@gmail.com>
2024-02-13 21:12:32 +0600

    branch 3.3 is done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,69 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable react/no-unescaped-entities */
-const Form = ({ status }) => {
-    // visual states: empty, typing, submitting, success, error
+/* eslint-disable react/prop-types */
 
+import { useState } from "react";
+
+function submitForm(answer) {
+    // Pretend it's hitting the network.
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (answer.toLowerCase() === "dhaka") {
+                resolve();
+            } else {
+                reject(new Error("Good guess but a wrong answer. Try again!"));
+            }
+        }, 3000);
+    });
+}
+
+const Form = () => {
+    // visual states: typing, submitting, success
+
+    const [answer, setAnswer] = useState("");
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState("typing");
+
+    const handelTextChange = async (e) => {
+        setError(null);
+        setAnswer(e.target.value);
+    };
+
+    const handelSubmit = async (e) => {
+        e.preventDefault();
+        setStatus("submitting")
+        try {
+            setStatus("success")
+            console.log(answer);
+            await submitForm(answer);
+        } catch (err) {
+            setStatus("typing")
+            setError(err.message);
+        }
+    };
+
+    
     if (status === "success") return <h1 className="mt-5">That's right!</h1>;
+
     return (
         <>
             <h2>City quiz</h2>
             <p>What city is located on two continents?</p>
-            <form>
+            <form onSubmit={handelSubmit}>
                 <textarea
+                    value={answer}
+                    onChange={handelTextChange}
+                    className="border"
                     disabled={status === "submitting"}
-                    className="border "
                 ></textarea>
                 <br />
                 <button
-                    disabled={status === "empty" || status === "submitting"}
+                    disabled={answer.length === 0 || status === "submitting"}
                     className="px-3 py-1 border"
                 >
                     Submit
                 </button>
                 {status === "submitting" && <p>Loading...</p>}
-                {status === "error" && (
-                    <p style={{ color: "red" }}>
-                        Good guess but a wrong answer. Try again !!!
-                    </p>
-                )}
+                {error && <p style={{ color: "red" }}>{error}</p>}
             </form>
         </>
     );
